22ccfc2c5fcba4da39346a05fadd000dc4bf8962
ohtomi <ohtomi.kenichi@gmail.com>
2016-01-16 00:09:44 +0900

    Use groovy-handlebars-engine for waf-java-app

--- templates/waf-java-app/lazybones.groovy
+++ templates/waf-java-app/lazybones.groovy
@@ -1,3 +1,8 @@
+@Grab(group="uk.co.cacoethes", module="groovy-handlebars-engine", version="0.2")
+import uk.co.cacoethes.handlebars.HandlebarsTemplateEngine
+
+registerDefaultEngine new HandlebarsTemplateEngine()
+
 def props = [:]
 props.app_name = ask("Define value for 'name' [java-app]: ", 'java-app', 'app_name')
 props.app_version = ask("Define value for 'version' [0.1]: ", '0.1', 'app_version')
