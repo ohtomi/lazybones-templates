3a6b1cce0ebefde870b4c8602b6ac7d6e5a81f97
ohtomi <ohtomi.kenichi@gmail.com>
2016-08-27 22:37:32 +0900

    Add template project for kotlin library

---
+++ templates/gradle-kotlin-lib/build.gradle.kts
@@ -0,0 +1,54 @@
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.dokka.gradle.LinkMapping
+
+
+buildscript {
+    extra["kotlin_version"] = "1.0.3"
+    extra["dokka_version"] = "0.9.9"
+
+    repositories {
+        gradleScriptKotlin()
+        jcenter()
+    }
+
+    dependencies {
+        classpath(kotlinModule("gradle-plugin"))
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:${extra["dokka_version"]}")
+    }
+}
+
+apply {
+    plugin("kotlin")
+    plugin("org.jetbrains.dokka")
+}
+
+
+repositories {
+    gradleScriptKotlin()
+    jcenter()
+}
+
+dependencies {
+    compile(kotlinModule("stdlib", extra["kotlin_version"] as String))
+}
+
+
+group = project.properties["lib_group"] as String
+version = project.properties["lib_version"] as String
+
+tasks.withType<Jar> {
+    baseName = project.properties["lib_name"] as String
+
+    manifest.apply {
+        attributes["Implementation-Title"] = project.properties["lib_name"]
+        attributes["Implementation-Version"] = project.properties["lib_version"]
+    }
+}
+
+
+tasks.withType<DokkaTask> {
+    outputFormat = "javadoc"
+    outputDirectory = "${buildDir.absolutePath}/javadoc"
+    sourceDirs = files("src/main/kotlin")
+}
