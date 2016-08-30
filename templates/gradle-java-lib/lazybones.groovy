78fe170ecb276e6de58eb6453e4c91a0dc6a724f
ohtomi <ohtomi.kenichi@gmail.com>
2016-08-31 02:38:22 +0900

    Use target directory as default value of project name

--- templates/gradle-java-lib/lazybones.groovy
+++ templates/gradle-java-lib/lazybones.groovy
@@ -1,5 +1,5 @@
 def props = [:]
-props.lib_name = ask("Define value for 'name' [java-lib]: ", 'java-lib', 'lib_name')
+props.lib_name = ask("Define value for 'name' [${projectDir.name}]: ", projectDir.name, 'lib_name')
 props.lib_group = ask("Define value for 'group' [org.example]: ", 'org.example', 'lib_group')
 props.lib_version = ask("Define value for 'version' [0.1]: ", '0.1', 'lib_version')
 
