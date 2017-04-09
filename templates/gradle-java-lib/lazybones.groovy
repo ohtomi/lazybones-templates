a2f4698cf6516b4f39d290a1ea5faba52e2c26c7
ohtomi <ohtomi.kenichi@gmail.com>
2017-04-09 23:13:48 +0900

    [gradle-java-lib] Tweak ask message

--- templates/gradle-java-lib/lazybones.groovy
+++ templates/gradle-java-lib/lazybones.groovy
@@ -1,7 +1,7 @@
 def props = [:]
-props.lib_name = ask("Define value for 'name' [${projectDir.name}]: ", projectDir.name, 'lib_name')
-props.lib_group = ask("Define value for 'group' [org.example]: ", 'org.example', 'lib_group')
-props.lib_version = ask("Define value for 'version' [0.1]: ", '0.1', 'lib_version')
+props.lib_name = ask("name: (${projectDir.name}) ", projectDir.name)
+props.lib_group = ask("group: (org.example) ", 'org.example')
+props.lib_version = ask("version: (0.1) ", '0.1')
 
 processTemplates 'README.md', props
 processTemplates 'gradle.properties', props
