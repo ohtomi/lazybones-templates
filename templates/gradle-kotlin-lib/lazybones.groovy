abee61507821177d726f03ad801e3ff8da7732ad
ohtomi <ohtomi.kenichi@gmail.com>
2018-12-14 00:56:53 +0900

    [gradle-kotlin-lib] Fix buildSrc/.gitignore bug

--- templates/gradle-kotlin-lib/lazybones.groovy
+++ templates/gradle-kotlin-lib/lazybones.groovy
@@ -5,16 +5,16 @@ println 'Press ^C at any time to quit.'
 println ''
 println '[Maven artifact]'
 props.artifact_name = ask("name: (${projectDir.name}) ", projectDir.name, 'artifact_name')
-props.artifact_group = ask("group: (org.example) ", 'org.example', 'artifact_group')
+props.artifact_group = ask('group: (org.example) ', 'org.example', 'artifact_group')
 
 println ''
 println '[GitHub repository]'
-props.repo_owner = ask("owner: (${System.getenv('USER')}) ", System.getenv('USER'))
-props.repo_name = ask("name: (${projectDir.name}) ", projectDir.name)
+props.repo_owner = ask("owner: (${System.getenv('USER')}) ", System.getenv('USER'), 'repo_owner')
+props.repo_name = ask("name: (${projectDir.name}) ", projectDir.name, 'repo_name')
 
 println ''
-println '[Kotlin]'
-props.is_application = ask("application? (no) [yes/no] ", "no") ==~ /(?i)y(es)?/
+println '[App or Lib]'
+props.is_application = ask('application? (no) [yes/no] ', 'no') ==~ /(?i)y(es)?/
 
 props.copyright_name = System.getenv('GITHUB_COPYRIGHT_NAME') ?: 'COPYRIGHT_NAME'
 props.copyright_year = Calendar.getInstance().get(Calendar.YEAR)
@@ -25,7 +25,9 @@ processTemplates 'settings.gradle.kts', props
 processTemplates 'build.gradle.kts', props
 processTemplates 'gradle.properties', props
 
-def gitignore = new File(projectDir.getAbsolutePath() + '/gitignore')
-if (gitignore.exists()) {
-    gitignore.renameTo(projectDir.getAbsolutePath() + '/.gitignore')
+[new File("${projectDir.getAbsolutePath()}/gitignore"),
+ new File("${projectDir.getAbsolutePath()}/buildSrc/gitignore")].each { gitignore ->
+    if (gitignore.exists()) {
+        gitignore.renameTo("${gitignore.getParent()}/.gitignore")
+    }
 }
