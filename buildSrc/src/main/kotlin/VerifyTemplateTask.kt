35c592441c34451c93a82d8d7356620fc99945ed
ohtomi <ohtomi.kenichi@gmail.com>
2018-12-18 22:53:18 +0900

    Report progress while verifying template

--- buildSrc/src/main/kotlin/VerifyTemplateTask.kt
+++ buildSrc/src/main/kotlin/VerifyTemplateTask.kt
@@ -2,8 +2,8 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskExecutionException
+import java.io.File
 import java.io.IOException
-import java.nio.charset.StandardCharsets
 import java.util.concurrent.TimeUnit
 
 open class VerifyTemplateTask : DefaultTask() {
@@ -26,26 +26,62 @@ open class VerifyTemplateTask : DefaultTask() {
             throw TaskExecutionException(this, IllegalStateException("No test cases for $templateName $templateVersion"))
         }
 
-        // see http://oswald.hatenablog.com/entry/20100426/1272240082
+        var success = 0
         testCases.forEachIndexed { index: Int, item: VerifyTemplateConventionItem ->
+            println("\n${index + 1} of ${testCases.size} $WALKING_EMOJI ...")
+
             // TODO clean destBaseDir before running lazybones
             val destDir = "$destBaseDir/$index"
-            val commands = arrayOf("lazybones", "create", templateName, templateVersion, destDir, *item.params)
-            ProcessBuilder(*commands).run {
-                try {
-                    val process = start()
-                    process.waitFor(3, TimeUnit.SECONDS)
-                    if (process.isAlive) {
-                        process.destroy()
-                    }
-                    println(process.errorStream.readAllBytes().toString(StandardCharsets.UTF_8))
-                    // TODO close all streams of process
-                } catch (e: IOException) {
 
-                } catch (e: InterruptedException) {
-
+            try {
+                val createCommands = arrayOf("lazybones", "create", templateName, templateVersion, destDir, *item.params)
+                val createReturnCode = runExternalProcess(createCommands, project.rootDir, 3, TimeUnit.SECONDS)
+                if (createReturnCode != 0) {
+                    println("\n$SCREAM_EMOJI  Failed to generate a new project.")
+                    return@forEachIndexed
                 }
+                println("$THUMBS_UP_EMOJI  A new project was generated in $destDir")
+
+                val buildCommands = arrayOf("./gradlew", "clean", "build", "syncLibs", "bintrayUpload")
+                val buildReturnCode = runExternalProcess(buildCommands, project.file(destDir), 30, TimeUnit.SECONDS)
+                if (buildReturnCode != 0) {
+                    println("$SCREAM_EMOJI  Failed to build the project.")
+                    return@forEachIndexed
+                }
+                println("$THUMBS_UP_EMOJI  The project was completely built.")
+
+                success++
+            } catch (e: IOException) {
+                println("\n$SCREAM_EMOJI  Exception: ${e.message}")
+                return@forEachIndexed
+            } catch (e: InterruptedException) {
+                println("\n$SCREAM_EMOJI  Exception: ${e.message}")
+                return@forEachIndexed
             }
+        }
+
+        if (success < testCases.size) {
+            throw TaskExecutionException(this, IllegalStateException("Template $templateName:$templateVersion has some errors."))
+        }
+    }
+
+    @Throws(IOException::class, InterruptedException::class)
+    fun runExternalProcess(commands: Array<String>, directory: File, timeout: Long, unit: TimeUnit): Int = ProcessBuilder().run {
+        command(*commands)
+        directory(directory)
+        start().run {
+            waitFor(timeout, unit)
+            if (isAlive) {
+                destroyForcibly()
+                waitFor(1, TimeUnit.SECONDS)
+            }
+            exitValue()
         }
     }
 }
+
+val WALKING_EMOJI = String(Character.toChars(0x1F6B6))
+
+val SCREAM_EMOJI = String(Character.toChars(0x1F631))
+
+val THUMBS_UP_EMOJI = String(Character.toChars(0x1F44D))
